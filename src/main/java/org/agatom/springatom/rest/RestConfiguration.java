273b6d2561bdeafa5ab122faff2d8a27b92a9db1
Tomasz Trębski <kornicameister@gmail.com>
2015-06-30 23:48:57 +0200

    - fixed test not working

--- src/main/java/org/agatom/springatom/rest/RestConfiguration.java
+++ src/main/java/org/agatom/springatom/rest/RestConfiguration.java
@@ -1,9 +1,12 @@
 package org.agatom.springatom.rest;
 
+import org.springframework.boot.autoconfigure.data.rest.SpringBootRepositoryRestMvcConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
 @ComponentScan
+@Import(SpringBootRepositoryRestMvcConfiguration.class)
 public class RestConfiguration {
 }
--- src/test/java/org/agatom/springatom/SpringAtomApplicationTests.java
+++ src/test/java/org/agatom/springatom/SpringAtomApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = SpringAtomApplication.class)
-public class SpringAtomATests {
+public class SpringAtomApplicationTests {
   @Test
   public void contextLoads() {
   }
